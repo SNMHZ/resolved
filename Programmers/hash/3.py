c54ae7f6bbb6346c99d229ea83869a3a87544991
SNMHZ <62589993+SNMHZ@users.noreply.github.com>
2020-07-31 22:40:37 +0900

    Add files via upload

---
+++ Programmers/hash/3.py
@@ -0,0 +1,15 @@
+def solution(clothes):
+    answer = 1
+    clothe_dict={}
+    
+    for cloth in clothes:
+        if cloth[1] not in clothe_dict:
+            clothe_dict[cloth[1]]=[]
+            clothe_dict[cloth[1]].append(cloth[0])
+        else:
+            clothe_dict[cloth[1]].append(cloth[0])
+    
+    for gen in clothe_dict.keys():
+        answer*=(len(clothe_dict[gen])+1)
+    
+    return answer-1
