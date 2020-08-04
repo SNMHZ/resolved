82f3ad7d5ad493bf6834de956082631382381a94
SNMHZ <62589993+SNMHZ@users.noreply.github.com>
2020-08-05 00:12:34 +0900

    Add files via upload

---
+++ Programmers/brute-force/3.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> get_expects(int size){
+    vector<vector<int>> expects;
+    for(int i=1; i<size/2; i++){
+        if(!(size%i)){
+            if(size/i<i)
+                break;
+            expects.push_back(vector<int>({size/i, i}));
+        }    
+    }
+    return expects;
+}
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+    vector<vector<int>> expects=get_expects(brown+yellow);
+    
+    for(auto k:expects){
+        if( (k[0]-2)*(k[1]-2)==yellow ){
+            answer.assign(k.begin(), k.end());
+            break;
+        }
+    }
+    
+    return answer;
+}
