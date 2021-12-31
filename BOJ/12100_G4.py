6b020852638dd5571f2bd2442878157412c1f8d7
unknown <snmhz325@gmail.com>
2021-12-31 15:36:23 +0900

    boj

---
+++ BOJ/12100_G4.py
@@ -0,0 +1,7 @@
+#2048 으익 귀찮아
+import sys
+
+N = int(sys.stdin.readline())
+board = [ list(map(int, sys.stdin.readline().split())) for _ in range(N) ]
+
+print(board)
--- BOJ/23291_G1.py
+++ BOJ/23291_G1.py
@@ -1,31 +1,49 @@
-#으악 귀찮아
 import sys
 
 N, K = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
 
-snail_map = [[-1] * N for _ in range(N)]
+board = [[-1] * N for _ in range(N)]
 snail_idx = [None] * N
+half_idx = [None] * N
+
+# snail_fish() init
 for i in range(N):
-    snail_map[0][i] = i
+    board[0][i] = i
+s_offset = 0
+sy, sx = 1, 1
+while sy <= (N-sy*sx):
+    for y in range(sy):
+        for x in range(sx):
+            board[sx-x][s_offset+sx+y] = board[y][s_offset+x]
+            board[y][s_offset+x] = -1
+    s_offset+=sx
+    sy, sx = sx+1, sy
+for y in range(sy):
+    for x in range(s_offset, N):
+        if board[y][x] != -1:
+            snail_idx[board[y][x]] = [y, x]
+            board[y][x] = -1
 
-offset = 0
-oy, ox = 1, 1
+# half_fish() init
+for i in range(N):
+    board[0][i] = i
+h_offset = 0
+hy, hx = 1, N//2
+for i in range(2):
+    for y in range(hy):
+        for x in range(hx):
+            board[(hy*2)-1-y][N-1-x] = board[y][h_offset+x]
+            board[y][h_offset+x] = -1
+    h_offset+=hx
+    hy, hx = hy*2, hx//2
+for y in range(hy):
+    for x in range(h_offset, N):
+        if board[y][x] != -1:
+            half_idx[board[y][x]] = [y, x]
+            board[y][x] = -1
 
-while oy <= (N-oy*ox):
-    print(offset)
-    for i in range(ox):
-        for j in range(oy):
-            snail_map[ox-i][offset+ox+j] = snail_map[j][offset+i]
-            snail_map[j][offset+i] = -1
-    offset+=ox
-    oy, ox = ox+1, oy
-
-for y in range(oy):
-    for x in range(offset, N):
-        if snail_map[y][x] != -1:
-            snail_idx[snail_map[y][x]] = [y, x]
-snail_map[0] = arr
+board[0] = arr
 
 # 젤 적은거에 하나씩
 def add_fish():
@@ -39,40 +57,59 @@ def add_fish():
         arr[i] += 1
 
 # 달팽이 만들기 & 정리 & 펼치기
-snail_mod = [ [0]*N for _ in range(N) ]
+mod_board = [ [0]*N for _ in range(N) ]
 def snail_fish():
+    #  달팽이 만들기
     for k, (y, x) in enumerate(snail_idx):
-        snail_map[y][x] = snail_map[0][k]
+        board[y][x] = board[0][k]
         if y != 0:
-            snail_map[0][k] = -1
-    
-    for y in range(oy):
-        for x in range(offset, N):
-            if snail_map[y][x] != -1:
-                print(snail_map[y][x])
+            board[0][k] = -1
+    # 정리
+    for y in range(sy):
+        for x in range(s_offset, N):
+            if board[y][x] != -1:
                 for dy, dx in ((1, 0), (0, 1), (-1, 0), (0, -1)):
                     ny, nx = y+dy, x+dx
-                    if nx<N and snail_map[ny][nx] != -1:
-                        cur_mod = abs(snail_map[ny][nx]-snail_map[y][x])//5
-                        cur_mod *= (1 if snail_map[ny][nx] > snail_map[y][x] else -1)
-                        snail_mod[y][x] += cur_mod
-    for y in range(oy):
-        for x in range(offset, N):
-            snail_map[y][x] += snail_mod[y][x]
-            snail_mod[y][x] = 0
-
-    print()
-    for row in snail_mod:
-        print(row)
-
-    print()
-    for row in snail_map:
-        print(row)
-
+                    if 0<=nx<N and 0<=ny<N and board[ny][nx] != -1:
+                        cur_mod = abs(board[ny][nx]-board[y][x])//5
+                        cur_mod *= (1 if board[ny][nx] > board[y][x] else -1)
+                        mod_board[y][x] += cur_mod
+    for y in range(sy):
+        for x in range(s_offset, N):
+            board[y][x] += mod_board[y][x]
+            mod_board[y][x] = 0
+    # 펼치기
+    for y in range(sy):
+        for x in range(s_offset, s_offset+sx):
+            board[0][(x-s_offset)*sy+y] = board[y][x]
+            board[y][x] = -1
 
 # 절반 두번 쌓기 & 정리 & 펼치기
 def half_fish():
-    pass
+    # 절반 두번 쌓기
+    for k, (y, x) in enumerate(half_idx):
+        board[y][x] = board[0][k]
+        if y != 0:
+            board[0][k] = -1
+    # 정리
+    for y in range(hy):
+        for x in range(h_offset, N):
+            if board[y][x] != -1:
+                for dy, dx in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+                    ny, nx = y+dy, x+dx
+                    if 0<=nx<N and 0<=ny<N and board[ny][nx] != -1:
+                        cur_mod = abs(board[ny][nx]-board[y][x])//5
+                        cur_mod *= (1 if board[ny][nx] > board[y][x] else -1)
+                        mod_board[y][x] += cur_mod
+    for y in range(hy):
+        for x in range(h_offset, N):
+            board[y][x] += mod_board[y][x]
+            mod_board[y][x] = 0
+    # 펼치기
+    for y in range(hy):
+        for x in range(h_offset, N):
+            board[0][(x-h_offset)*hy+y] = board[y][x]
+            board[y][x] = -1
 
 # 차이 K 이하인지 체크
 count = 0
@@ -83,7 +120,5 @@ while True:
     add_fish()
     snail_fish()
     half_fish()
-    break
 
 print(count)
-
