1413990c581a5f152ba3307cb43225288e6bd948
unknown <snmhz325@gmail.com>
2022-01-04 14:26:21 +0900

    boj

--- BOJ/15551_G3.py
+++ BOJ/15551_G3.py
@@ -1,19 +1,18 @@
 import sys
 
+# def hh(a):
+#         h = 0
+#         for i in range(N):
+#             h = 31*h + ord(a[i])
+#         return h
+
+# for N in range(2, 101):
+#     A = 's'*(N-2)+'Ao'
+#     B = 's'*(N-2)+'BP'
+
+#     assert hh(A) == hh(B)
+
 N = int(sys.stdin.readline())
 
-a = 'a'*N
-
-h = 0
-for i in range(N):
-    h = 31*h + ord(a[i])
-
-b = ''
-for i in range(N-1):
-    b += chr(h%26+ord('b'))
-    h = h//26-ord('b')
-
-b += chr(h)
-print(chr(h))
-
-print(b, h)
+print('s'*(N-2)+'Ao')
+print('s'*(N-2)+'BP')
