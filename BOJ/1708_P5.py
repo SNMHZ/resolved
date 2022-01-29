c60d2d2152191c40aa3b93326bfd5bd31c35e48c
unknown <snmhz325@gmail.com>
2022-01-29 17:32:38 +0900

    boj

--- BOJ/1708_P5.py
+++ BOJ/1708_P5.py
@@ -2,29 +2,23 @@ import sys
 from functools import cmp_to_key
 
 N = int(sys.stdin.readline())
-points = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+points = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-# graham scan
-def ccw(a, b, c):
-    return a[0]*b[1] + b[0]*c[1] + c[0]*a[1] - b[0]*a[1] - c[0]*b[1] - a[0]*c[1]
-
+# convex_hull. graham scan
 def convex_hull(points):
     _points = sorted(points, key=lambda x: (x[1], x[0]))
     stack = [_points[0]]
+    def _ccw(a, b, c): return a[0]*b[1] + b[0]*c[1] + c[0]*a[1] - b[0]*a[1] - c[0]*b[1] - a[0]*c[1]
+    def _dist(a, b): return (a[0]-b[0])**2 + (a[1]-b[1])**2
     def _cmp(a, b):
-        c = ccw(_points[0], a, b)
-        if c != 0:
-            return c < 0
-        return (a[0] - _points[0][0])**2 + (a[1] - _points[0][1])**2 > (b[0] - _points[0][0])**2 + (b[1] - _points[0][1])**2
-
+        c = _ccw(_points[0], a, b)
+        if c != 0: return -c
+        return _dist(a, _points[0]) - _dist(b, _points[0])
     _points = sorted(_points[1:], key=cmp_to_key(_cmp))
-    print(_points)
     for i in range(len(_points)):
-        while len(stack) >= 2 and ccw(_points[i], stack[-2], stack[-1]) <= 0:
-            print(stack)
-            stack.pop()
+        while len(stack) >= 2 and _ccw(_points[i], stack[-2], stack[-1]) <= 0: stack.pop()
         stack.append(_points[i])
+    
     return stack
 
-print(convex_hull(points))
 print(len(convex_hull(points)))
--- BOJ/2254_P4.py
+++ BOJ/2254_P4.py
@@ -4,43 +4,30 @@ from functools import cmp_to_key
 N, px, py = map(int, sys.stdin.readline().split())
 dots = set([tuple(map(int, sys.stdin.readline().split())) for _ in range(N)])
 
-
-
-def dotmul(a, b):
-    return a[0]*b[1] - a[1]*b[0]
-
-def dotsub(a, b):
-    return (a[0]-b[0], a[1]-b[1])
-
-def cmp(a, b):
-    if dotmul(a, b):
-        return dotmul(a, b) < 0
-    return (a[0]**2 + a[1]**2) < (b[0]**2 - b[1]**2)
-
-def convex_hull(dots):
-    dots = sorted(dots)
-    _dots = [(x-dots[0][0], y-dots[0][1]) for x, y in dots]
-    stack = [_dots[0], _dots[1]]
-    _dots = sorted(_dots[2:], key=cmp_to_key(cmp))
+# convex_hull. graham scan
+def convex_hull(points):
+    _points = sorted(points, key=lambda x: (x[1], x[0]))
+    stack = [_points[0]]
+    def _ccw(a, b, c): return a[0]*b[1] + b[0]*c[1] + c[0]*a[1] - b[0]*a[1] - c[0]*b[1] - a[0]*c[1]
+    def _dist(a, b): return (a[0]-b[0])**2 + (a[1]-b[1])**2
+    def _cmp(a, b):
+        c = _ccw(_points[0], a, b)
+        if c != 0: return -c
+        return _dist(a, _points[0]) - _dist(b, _points[0])
+    _points = sorted(_points[1:], key=cmp_to_key(_cmp))
+    for i in range(len(_points)):
+        while len(stack) >= 2 and _ccw(_points[i], stack[-2], stack[-1]) <= 0: stack.pop()
+        stack.append(_points[i])
     
-    for i in range(len(_dots)):
-        while len(stack) >= 2 and dotmul(dotsub(stack[-1], stack[-2]), dotsub(stack[-1], _dots[i])) < 0:
-            stack.pop()
-        stack.append(_dots[i])
-    return [(x+dots[0][0], y+dots[0][1]) for x, y in stack]
-
-def ccw(a, b, c):
-    cw = (a[0]*b[1]+b[0]*c[1]+c[0]*a[1]) - (a[1]*b[0]+b[1]*c[0]+c[1]*a[0])
-    if cw > 0:
-        return 1
-    elif cw < 0:
-        return -1
-    else:
-        return 0
+    return stack
 
 def isInside(dots, x, y) -> bool:
-    if len(dots) < 3:
-        return False
+    if len(dots) < 3: return False
+    def ccw(a, b, c):
+        cw = (a[0]*b[1]+b[0]*c[1]+c[0]*a[1]) - (a[1]*b[0]+b[1]*c[0]+c[1]*a[0])
+        if cw > 0: return 1
+        elif cw < 0: return -1
+        else: return 0
     cwb = ccw(dots[0], dots[1], (x, y))
     for i in range(1, len(dots)):
         cw = ccw(dots[i-1], dots[i], (x, y))
@@ -54,11 +41,9 @@ while True:
         cur_convex = convex_hull(dots)
     else:
         break
-    print(cur_convex)
     if isInside(cur_convex, px, py):
         cnt += 1
         dots-=set(cur_convex)
-        print(dots)
     else:
         break
 
