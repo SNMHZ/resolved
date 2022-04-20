74269b7e989789d49524b3a9965ef81de3673c93
SNMHZ <snmhz325@gmail.com>
2022-04-20 13:32:33 +0900

    boj

---
+++ BOJ/11000.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+
+N = int(sys.stdin.readline())
+lessons = [ list(map(int, sys.stdin.readline().split())) for _ in range(N) ]
+rooms = []
+
+lessons.sort(key=lambda x: x[0])
+
+for i in range(N):
+    if len(rooms) != 0 and rooms[0]<= lessons[i][0]:
+        heapq.heappop(rooms)
+    heapq.heappush(rooms, lessons[i][1])
+
+print(len(rooms))
